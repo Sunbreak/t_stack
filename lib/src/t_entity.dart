4e1dfbb64f787ae55f449713f19a9cc10a9bffae
Sunbreak <sunbreak.wang@gmail.com>
2021-08-31 14:51:08 +0800

    feat: Add TNodeManager & TNavigator
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

--- lib/src/t_entity.dart
+++ lib/src/t_entity.dart
@@ -1,14 +1,24 @@
+const kTNode = 'node';
+const kTypeFlutter = 0;
+const kTypeNative = 1;
+
 class TNode {
-  // final String identifier;
+  final String id;
 
   final String routeName;
 
+  final int type;
+
+  TNode(this.id, this.routeName, this.type);
+
   TNode.fromMap(Map<String, dynamic> map)
-      // : identifier = map['identifier'],
-      : routeName = map['routeName'];
+      : id = map['id'],
+        routeName = map['routeName'],
+        type = map['type'];
 
   Map<String, dynamic> toMap() => {
-        // 'identifier': identifier,
+        'id': id,
         'routeName': routeName,
+        'type': type,
       };
 }
